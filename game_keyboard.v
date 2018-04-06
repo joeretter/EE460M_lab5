e9e0823d25c6614a2faa2b40a2343001762ece14
Joe Retter <joeretter@utexas.edu>
2018-04-06 15:29:41 -0500

    added single pulse to game_keyboard

--- game_keyboard.v
+++ game_keyboard.v
@@ -70,3 +70,46 @@ begin
 end
 
 endmodule
+
+module AND(a, b, out);
+input a, b;
+output out;
+
+assign out = a & b;
+
+endmodule
+
+module DFF(clk, d, q, q_bar);
+input clk, d;
+output q, q_bar;
+
+reg q, q_bar;
+
+always @(posedge clk)
+begin
+  q <= d;
+  q_bar <= ~d;
+end
+
+endmodule
+
+
+module debounce(clk, D, SYNCPRESS);
+input clk, D;
+output SYNCPRESS;
+
+DFF flop1(clk, D, flop1_Q, unused1);
+DFF flop2(clk, flop1_Q, SYNCPRESS, unused2);
+
+endmodule
+
+
+module single_pulse(clk, press, SP);
+input clk, press;
+output SP;
+
+debounce debouncer(clk, press, sync_press);
+DFF flip_flop(clk, sync_press, unused, q_bar);
+AND and_gate(sync_press, q_bar, SP);
+
+endmodule
